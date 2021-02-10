0c4e85a763eff6d136904dbf5c62c228ed54bddb
f.X Blanson <fat_tonyy@icloud.com>
2021-02-10 19:28:00 +0000

    wip: testing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,10 @@ import "./HeaderStyles.scss";
 function Header() {
 	return (
 		<div>
+			<h2>damn you</h2>
+			<h1>damn you</h1>
+			<h1>damn you</h1>
+			<h1>damn you</h1>
 			<h1>damn you</h1>
 		</div>
 	);
