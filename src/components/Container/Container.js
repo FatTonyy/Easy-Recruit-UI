2ed37143330d4a1cbba96d97ab28b6a6865a7a9d
f.X Blanson <fat_tonyy@icloud.com>
2021-02-10 15:43:54 +0000

    wip: workstation-setup

---
+++ src/components/Container/Container.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import ContainerSCSS from "./ContainerStyles.scss";
+import "./ContainerStyles.scss";
+
+function Container() {
+	return (
+		<div>
+			{" "}
+			<h3>goodbye</h3>
+		</div>
+	);
+}
+
+export default Container;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 // personalized modules
 
 import Header from "./components/Header/Header";
+import Container from "./components/Container/Container";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Container />
 		<Header />
 	</React.StrictMode>,
 	document.getElementById("root")
