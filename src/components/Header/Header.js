a22c536114006a26f37826d29eab6092bef74d92
f.X Blanson <fat_tonyy@icloud.com>
2021-02-19 11:55:37 +0000

    wip: nav-section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,36 @@
 import React from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faBars, faTimes } from "@fortawesome/fontawesome";
 import "./HeaderStyles.scss";
 
 function Header() {
 	return (
-		<nav id="main-nav">
+		<header className="header">
 			<div className="container">
-				<img src="../Images/logoeasy.png" className="logo" alt="company logo" />
-				<ul>
-					<li>
-						<a className="btn btn-1" href="https://www.google.com">
-							CV Writing
-						</a>{" "}
-					</li>
-					<li>
-						<a className="btn" href="https://www.google.com">
-							Home
-						</a>{" "}
-					</li>
-				</ul>
+				<nav id="main-nav">
+					<a href="https://www.google.com" className="logo">
+						<img src="../Images/logoeasy.png" alt="company logo" />
+					</a>
+					{/* hamburger icons below */}
+					<div class="hamburger-menu">
+						{/* <FontAwesomeIcon icon={faBars} /> */}
+						{/* <FontAwesomeIcon icon={faTimes} /> */}
+					</div>
+					<ul className="nav-list">
+						<li className="nav-item">
+							<a className="nav-link" href="https://www.google.com">
+								CV Writing
+							</a>{" "}
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="https://www.google.com">
+								Home
+							</a>{" "}
+						</li>
+					</ul>
+				</nav>
 			</div>
-		</nav>
+		</header>
 	);
 }
 
